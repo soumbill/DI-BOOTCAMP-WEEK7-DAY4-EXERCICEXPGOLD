ca8118b6ff463a7d4078257d092b34640aa8fa1d
soumbill <konesoumailabilly@gmail.com>
2023-02-13 03:55:20 +0100

    ExoXPGOLD commit

---
+++ exercice3/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Program to calculate electricity bill in PHP</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body><br>
+<div class="container">
+	<h1>exercice3 </h1>
+		<?php
+		echo "La fonction require() de PHP est essentiellement utilisée pour inclure le contenu/code/données
+		 d'un fichier PHP dans un autre fichier PHP. Au cours de ce processus, s'il y a des erreurs, cette fonction require ()
+		  affichera un avertissement avec une erreur fatale et arrêtera immédiatement l'exécution du script. Afin d'utiliser 
+		  cette fonction require() , nous devrons d'abord créer deux fichiers PHP <br /><br />";
+
+		  echo "La fonction include() de PHP est essentiellement utilisée pour inclure le contenu/code/données 
+		  d'un fichier PHP dans un autre fichier PHP. Au cours de ce processus, s'il y a des erreurs, 
+		  cette fonction include () affichera un avertissement, mais contrairement à la fonction require () ,
+		   elle n'arrêtera pas l'exécution du script, mais le script continuera son processus. Afin d'utiliser 
+		   cette fonction include() , nous devrons d'abord créer deux fichiers PHP. En utilisant la fonction include() ,
+		    incluez un fichier PHP dans un autre. Après cela, vous verrez deux fichiers PHP combinés en un seul fichier HTML. ";
+		?>
+		
+	</div>
+</body>
+</html>
